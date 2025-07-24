9b148c6eea8b46b45cf13e923bb4c8c1e5e04b0e
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-07-24 12:57:59 +0400

    Add button to return to the Home page

--- frontend/src/pages/ResultsPage.tsx
+++ frontend/src/pages/ResultsPage.tsx
@@ -108,6 +108,9 @@ export default function ResultsPage() {
           </div>
         </>
       ) : null}
+      <a href="/" className="button">
+        Analyze another website
+      </a>
     </div>
   );
 }
